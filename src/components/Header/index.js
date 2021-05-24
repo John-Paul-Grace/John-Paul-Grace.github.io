5ac0ab3f7f5e3c929cebc290e2a99df8e1e6b25f
JP-Grace <gracejohnpaul200@gmail.com>
2021-05-24 02:27:06 -0400

    header logos now hide at medium screen sizes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -12,20 +12,20 @@ import mongoLogo from "../../images/MongoDB-Logo.png";
 const Header = () => {
   return (
     <div className="flex flex-row gap-8 justify-center py-3">
-      <div className="grid grid-cols-2 grid-rows-2 w-1/6">
-        <img className="" src={htmlLogo} alt="HTML5 Logo" />
-        <img className="" src={jqueryLogo} alt="JQuery Logo" />
-        <img className="" src={tailwindLogo} alt="Tailwind Logo" />
-        <img className="" src={reactLogo} alt="React Logo" />
+      <div className="grid grid-cols-2 grid-rows-2 w-1/6 hidden md:grid">
+        <img src={htmlLogo} alt="HTML5 Logo" />
+        <img src={jqueryLogo} alt="JQuery Logo" />
+        <img src={tailwindLogo} alt="Tailwind Logo" />
+        <img src={reactLogo} alt="React Logo" />
       </div>
 
-      <img className="rounded-lg border-gray-500 border-8" src={jpgBrandLogo} width="50%" alt="Brand-Logo" />
+      <img className="rounded-lg border-gray-500 border-8 w-full md:w-1/2" src={jpgBrandLogo} alt="Brand-Logo" />
 
-      <div className="grid grid-cols-2 grid-rows-2 w-1/6">
+      <div className="grid grid-cols-2 grid-rows-2 w-1/6 hidden md:grid">
         <img className="self-center" src={nodeLogo} alt="Node Logo" />
         <img className="self-center pl-3" src={passportLogo} width="80%" alt="Passport Logo" />
-        <img className="" src={mysqlLogo} alt="MySQL Logo" />
-        <img className="" src={mongoLogo} alt="MongoDB Logo" />
+        <img src={mysqlLogo} alt="MySQL Logo" />
+        <img src={mongoLogo} alt="MongoDB Logo" />
       </div>
     </div>
   );
